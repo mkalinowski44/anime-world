96e515309f3c38dfd3f9a57a30ae89d332223dd0
Marcin Kalinowski <marcin36k@outlook.com>
2019-07-21 17:13:52 +0200

    Poprawienie headera w stronie z odcinkami anime

--- js/app/sidebars.js
+++ js/app/sidebars.js
@@ -1,5 +1,4 @@
 function Sidebar(page) {
-   console.log('x');
    this.page = page;
    this.container = document.querySelector('.page-container.display-flex');
 
@@ -10,21 +9,41 @@ function Sidebar(page) {
       this.button = document.querySelector('.page-container.display-flex .hide-show-right');
    }
 
+   this.calibrateButtonPosition = function(state) {
+      var width = this.container.offsetWidth;
+      if(width < 300) {
+         if(state === 1) {
+            if(this.page === 'left') {
+               this.button.style.transform = 'translateX(' + (width - 50) + 'px)';
+            }
+            if(this.page === 'right') {
+               this.button.style.transform = 'translateX(-' + (width - 50) + 'px)';
+            }
+         } else {
+            this.button.removeAttribute('style');
+         }
+      }
+   }.bind(this);
+
    this.buttonClick = function() {
       if(this.page === 'left') {
          if((" " + this.container.className + " ").replace(/[\n\t]/g, " ").indexOf(" expand-left ") > -1 ) {
             this.container.classList.remove('expand-left');
+            this.calibrateButtonPosition(0);
          } else {
             this.container.classList.add('expand-left');
             this.container.classList.remove('expand-right');
+            this.calibrateButtonPosition(1);
          }
       }
       if(this.page === 'right') {
          if((" " + this.container.className + " ").replace(/[\n\t]/g, " ").indexOf(" expand-right ") > -1 ) {
             this.container.classList.remove('expand-right');
+            this.calibrateButtonPosition(0);
          } else {
             this.container.classList.add('expand-right');
             this.container.classList.remove('expand-left');
+            this.calibrateButtonPosition(1);
          }
       }
    }.bind(this);
